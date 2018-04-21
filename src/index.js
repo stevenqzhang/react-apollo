03db081b28cae08a43d5f94e0c0ca6f2a099676e
Steven Zhang <git@stevenzhang.net>
2018-04-20 17:27:36 -0700

    get rid of auth

--- src/index.js
+++ src/index.js
@@ -15,19 +15,6 @@ import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({ uri: `http://localhost:4000` })
 
-const middlewareAuthLink = new ApolloLink((operation, forward) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
-  const authorizationHeader = token ? `Bearer ${token}` : null
-  operation.setContext({
-    headers: {
-      authorization: authorizationHeader,
-    },
-  })
-  return forward(operation)
-})
-
-const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
-
 const wsLink = new WebSocketLink({
   uri: `ws://localhost:4000`,
   options: {
@@ -38,13 +25,15 @@ const wsLink = new WebSocketLink({
   }
 })
 
+//https://www.apollographql.com/docs/link/
+//In a few words, Apollo Links are chainable “units” that you can snap together to define how each GraphQL request is handled by your GraphQL client. When you fire a GraphQL request, each Link’s functionality is applied one after another. This allows you to control the request lifecycle in a way that makes sense for your application. For example, Links can provide retrying, polling, batching, and more!
 const link = split(
   ({ query }) => {
     const { kind, operation } = getMainDefinition(query)
     return kind === 'OperationDefinition' && operation === 'subscription'
   },
   wsLink,
-  httpLinkWithAuthToken,
+  httpLink,
 )
 
 const client = new ApolloClient({
